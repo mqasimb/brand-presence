355777742a9574220b03753470ea0eb348061c30
bqasim381 <bqasim381@gmail.com>
2016-11-29 15:55:25 +0000

    initial deploy

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ var app = express();
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 8080);
+
+module.exports.app = app;
---
+++ test/test-server.js
@@ -0,0 +1,21 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server.js');
+
+var should = chai.should();
+var app = server.app;
+
+chai.use(chaiHttp);
+
+describe('Measure Brand Presence', function() {
+    it('should return a valid response code', function(done) {
+        chai.request(app)
+            .get('/')
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            });
+    });
+});
